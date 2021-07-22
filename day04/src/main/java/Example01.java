0ad874a7a7a00b04a5c5fcb267249be36cd1502a
tangaot <lmq0211@gmail.com>
2021-07-22 10:33:48 +0800

    add Day04

---
+++ day04/src/main/java/Example01.java
@@ -0,0 +1,101 @@
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Random;
+
+public class Example01 {
+        public static void main(String[] args) throws Exception {
+            StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+            env.setParallelism(1);
+
+            env
+                    .addSource(new ClickSource())
+                    .keyBy(r -> r.user)
+                    .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
+                    .aggregate(new CountAgg())
+                    .print();
+
+            env.execute();
+        }
+
+        public static class CountAgg implements AggregateFunction<Event, Integer, Integer> {
+
+            @Override
+            public Integer createAccumulator() {
+                return 0;
+            }
+
+            @Override
+            public Integer add(Event value, Integer accumulator) {
+                return accumulator+1;
+            }
+
+            @Override
+            public Integer getResult(Integer accumulator) {
+                return accumulator;
+            }
+
+            @Override
+            public Integer merge(Integer a, Integer b) {
+                return null;
+            }
+        }
+
+
+    // SourceFunction并行度只能为1
+    // 自定义并行化版本的数据源，需要使用ParallelSourceFunction
+    public static class ClickSource implements SourceFunction<Event> {
+        private boolean running = true;
+        private String[] userArr = {"Mary", "Bob", "Alice", "Liz"};
+        private String[] urlArr = {"./home", "./cart", "./fav", "./prod?id=1", "./prod?id=2"};
+        private Random random = new Random();
+        @Override
+        public void run(SourceContext<Event> ctx) throws Exception {
+            while (running) {
+                // collect方法，向下游发送数据
+                ctx.collect(
+                        new Event(
+                                userArr[random.nextInt(userArr.length)],
+                                urlArr[random.nextInt(urlArr.length)],
+                                Calendar.getInstance().getTimeInMillis()
+                        )
+                );
+                Thread.sleep(1000L);
+            }
+        }
+
+        @Override
+        public void cancel() {
+            running = false;
+        }
+    }
+
+    public static class Event {
+        public String user;
+        public String url;
+        public Long timestamp;
+
+        public Event() {
+        }
+
+        public Event(String user, String url, Long timestamp) {
+            this.user = user;
+            this.url = url;
+            this.timestamp = timestamp;
+        }
+
+        @Override
+        public String toString() {
+            return "Event{" +
+                    "user='" + user + '\'' +
+                    ", url='" + url + '\'' +
+                    ", timestamp=" + new Timestamp(timestamp) +
+                    '}';
+        }
+    }
+}
